25bdd27f74f361f43b8fc0e6e47b3cf672833b8a
Uros Preloznik <uros00@gmail.com>
2015-12-17 22:26:54 +0100

    Update 003_update.sql
    Remove user line from function

--- _scripts/003_update.sql
+++ _scripts/003_update.sql
@@ -31,6 +31,4 @@ $BODY$
   LANGUAGE plpgsql VOLATILE
   COST 1
   ROWS 1000;
-ALTER FUNCTION get_project_data(text)
-  OWNER TO pguser;
 COMMENT ON FUNCTION get_project_data(text) IS 'IN project --> client, theme, baselayers, overview layer, extra layers and tables_onstart for project_name.';
