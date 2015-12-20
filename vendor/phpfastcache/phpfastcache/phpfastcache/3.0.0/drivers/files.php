182f75d74d1189d2f3102ef6a7d861f880233909
uros <uros00@gmail.com>
2015-12-20 16:33:56 +0100

    update GuzzleHttp and PhpFastCache libraries

--- vendor/phpfastcache/phpfastcache/phpfastcache/3.0.0/drivers/files.php
+++ vendor/phpfastcache/phpfastcache/phpfastcache/3.0.0/drivers/files.php
@@ -118,7 +118,7 @@ class phpfastcache_files extends  BasePhpFastCache implements phpfastcache_drive
 
     function driver_delete($keyword, $option = array()) {
         $file_path = $this->getFilePath($keyword,true);
-        if(@unlink($file_path)) {
+        if(file_exists($file_path) && @unlink($file_path)) {
             return true;
         } else {
             return false;
@@ -162,7 +162,9 @@ class phpfastcache_files extends  BasePhpFastCache implements phpfastcache_drive
                             $key = $f;
                         }
                         else {
-                            $key = explode(".", $f)[0];
+                            //Because PHP 5.3, this cannot be written in single line
+                            $key = explode(".", $f);
+                            $key = $key[0];
                         }
                         $content[$key] = array("size"=>$size,"write_time"=>$object["write_time"]);
                         if($this->isExpired($object)) {
