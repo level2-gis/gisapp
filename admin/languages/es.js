78e04a34648a22c36ec537eb5f5fab1ab0d298d1
IgnacioSerranoAyllon <nachoserr@hotmail.com>
2016-04-26 17:37:55 +0200

    Create es.js
    spanish translation

---
+++ admin/languages/es.js
@@ -0,0 +1,41 @@
+/**
+ * English translation for administration part of Extended Qgis Web Client
+ *
+ * Translators: Ignacio Serrano
+ *
+ * Date: 2016-04-26
+ */
+var TR = {
+    appName: 'Spatial info. system - ',
+    loginTitle: 'Acceso',
+    loginMessage: 'Acceso para usuarios autentificados.' +
+    '<br />Por favor escriba su usuario y contrase\u00F1a.',
+    loginFailMessage: 'No se puede logear',
+    loginWaitMessage: 'Por favor, espere...',
+    loginButton: 'Login',
+    guestButton: 'Invitado',
+    loginUsernameLabel: 'Nombre de usuario',
+    loginPasswordLabel: 'Contrase\u00F1a',
+    loginLanguageLabel: 'Idioma',
+    loginRememberMeLabel: 'Recordarme en este ordenador',
+    loginForgotPasswordLabel: '',
+    logoutLabel: 'Desconectar',
+    noProject: 'El proyecto no existe.',
+    noProjectText: 'Añadir proyecto correcto y recargar la p\u00E1gina.',
+    wrongPassword: 'Contrase\u00F1a err\u00F1nea.',
+    noUser: 'El usuario no existe.',
+    noPermission: 'No tiene permiso para acceder a este proyecto.',
+    noPublicAccess: 'No est\u00E1 permitido el acceso p\u00F9 a este proyecto.',
+    clearFilter: 'Quitar todos los filtros',
+    menuFilterText: 'Filtros',
+    editData: 'Editar datos',
+    editDisabled: 'Edici\u00F3 deshabilitada',
+    editDelete: 'borrar',
+    editSave: 'Guardar',
+    mapBasic: 'Mapa',
+    mapSatellite: 'Sat\u00E9lite',
+    mapHybrid: 'Hybrido',
+    mapTerrain: 'Terreno',
+    fiLocation: 'Localizaci\u00F3n',
+    fiElevation: 'Sobre el nivel del mar'
+};
