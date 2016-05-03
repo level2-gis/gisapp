a96a2a40c74ba65c6f1d248765b9991a7c759790
uprel <uros00@gmail.com>
2016-05-03 20:40:37 +0200

    translation fix

--- admin/languages/_lang.js
+++ admin/languages/_lang.js
@@ -2,6 +2,6 @@ var gisAppLanguages = [
     ['sl', 'slovenščina', 'ux-flag-si'],
     ['en', 'English', 'ux-flag-en'],
     ['de', 'Deutsch', 'ux-flag-de'],
-    ['sk', 'slovenský', 'ux-flag-sk']
+    ['sk', 'slovenský', 'ux-flag-sk'],
 	['es', 'Español', 'ux-flag-es']
 ];
