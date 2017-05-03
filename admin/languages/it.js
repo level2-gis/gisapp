b658ca80866e93a13dc5b1c4741189b7d6902a40
ale12a <alessandro.sferrazza@ise-srl.it>
2017-05-03 20:11:34 +0200

    Master (#43)
    Italian translation
    
    Primo test di commit
    
    * Italian Translation
    
    * Test2

---
+++ admin/languages/it.js
@@ -0,0 +1,112 @@
+/**
+ * Italian translation for administration part of Extended Qgis Web Client
+ *
+ * Translators: Alessandro Sferrazza
+ *
+ * Date: 2017-05-03
+ */
+var TR = {
+    appName: 'Spatial info. system - ',
+    loginTitle: 'Login',
+    loginMessage: 'Accesso per utenti autorizzati.' +
+    '<br />Inserire nome utente e password.',
+    loginFailMessage: 'Impossibile fare il log in',
+    loginWaitMessage: 'Attendere...',
+    loginButton: 'Login',
+    guestButton: 'Guest',
+    loginUsernameLabel: 'Username',
+    loginPasswordLabel: 'Password',
+    loginLanguageLabel: 'Lingua',
+    loginRememberMeLabel: 'Ricordami su questo computer',
+    loginForgotPasswordLabel: '',
+    logoutLabel: 'Uscita',
+    noProject: 'Il progetto non esiste.',
+    noProjectText: 'Aggiungi un nome progetto corretto e ricarica la pagina.',
+    wrongPassword: 'Password errata.',
+    noUser: 'L\'utente non esiste.',
+    noPermission: 'Non hai i permessi per usare questo progetto.',
+    noPublicAccess: 'Accesso publico non permesso per questo progetto.',
+    clearFilter: 'Cancella tutti i filtri',
+    clearSelection: 'Cancella la selezione',
+    menuFilterText: 'Filtri',
+    editData: 'Modifica',
+    editMode: 'Modalità modifica',
+    editDisabled: 'Modifica disabilitata',
+    editDelete: 'Cancella',
+    editSave: 'Salva',
+    mapBasic: 'Mappa',
+    mapSatellite: 'Satellite',
+    mapHybrid: 'Ibrido',
+    mapTerrain: 'Terreno',
+    fiLocation: 'Luogo',
+    fiElevation: 'sul livello del mare',
+    cancel: 'Cancella',
+    transactionFail: 'Transazzione fallita',
+    show: 'Mostra',
+    properties: 'Proprietà'
+};
+
+/**
+ * Mobile Viewer translation strings
+ */
+
+var I18n = {};
+
+I18n.title = "Mobile Viewer";
+
+I18n.search = {
+    header: "Cerca",
+    results: "Risultati",
+    failed: "Ricerca fallita:"
+};
+
+I18n.properties = {
+    header: "Impostazioni Mappa",
+    mapFollowing: "Tracking",
+    mapRotation: "Auto Rotation",
+    scaleBar: "Scala",
+    about: "Imprint",
+    share: "Condividi",
+    login: "Login",
+    on: "On",
+    off: "Off"
+};
+
+I18n.about = {
+    header: "Imprint",
+    content: "Version sviluppo"
+};
+
+I18n.layers = {
+    topics: "Temi",
+    layers: "Layer",
+    layerOrder: "Ordine layer",
+    background: "Background",
+    overlays: "Overlays",
+    selection: "Selezione",
+    redlining: "Redlining",
+    transparency: "Transparenza"
+};
+
+I18n.featureInfo = {
+    header: "Informazioni",
+    feature: "Feature with ID:",
+    raster: "Raster",
+    noFeatureFound: "Nessuna Feature trovata"
+};
+
+I18n.geolocation = {
+    permissionDeniedMessage: "Location non disponibile.\n\nControlla il tuo browser o le impostazioni del device."
+};
+
+I18n.login = {
+    header: "Login",
+    user: "Utnte",
+    password: "Password",
+    signIn: "Login",
+    cancel: "Cancella",
+    signOut: "Logout",
+    signInFailed: "Username o password non validi",
+    signOutFailed: "Login fallito",
+    statusFailed: "Login Status fallito"
+};
