400bf2e08493a3fc3c107967f159cd96cefe73f3
uprel <uros00@gmail.com>
2017-07-13 16:17:41 +0200

    New views in database for gisportal

--- _scripts/_setup.sql
+++ _scripts/_setup.sql
@@ -616,3 +616,36 @@ ALTER TABLE ONLY projects
 -- PostgreSQL database dump complete
 --
 
+CREATE OR REPLACE VIEW public.clients_view AS
+  SELECT clients.id,
+    clients.name,
+    clients.display_name,
+    clients.url,
+    count(projects.id) AS count,
+    sort(array_agg(projects.id)) AS project_ids
+  FROM clients,
+    projects
+  WHERE clients.id = projects.client_id
+  GROUP BY projects.client_id, clients.id, clients.name, clients.display_name, clients.url;
+
+CREATE OR REPLACE VIEW public.projects_view AS
+  SELECT projects.id,
+    projects.name,
+    projects.client_id,
+    projects.public,
+    CASE
+    WHEN projects.display_name IS NULL THEN projects.name
+    ELSE projects.display_name
+    END AS display_name,
+    CASE
+    WHEN projects.crs IS NULL THEN '_'::text
+    ELSE projects.crs
+    END AS crs,
+    CASE
+    WHEN projects.description IS NULL THEN '_'::text
+    ELSE projects.description
+    END AS description,
+    clients.display_name AS client
+  FROM projects,
+    clients
+  WHERE projects.client_id = clients.id;
