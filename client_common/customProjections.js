75e0bdfb550ac12720ef590fba1795ae61962a82
uprel <uros00@gmail.com>
2017-08-05 18:52:56 +0200

    Added def. for  EPSG 32635

--- client_common/customProjections.js
+++ client_common/customProjections.js
@@ -10,7 +10,8 @@
 
 var CustomProj = [];
 
-CustomProj['EPSG:2180'] = {extent: [116649.52,118322.29,806302.07,845151.24], units: 'm', yx: true};
-CustomProj['EPSG:3006'] = {extent: [-1200000,4305696,2994304,8500000], units: 'm', yx: true};
-CustomProj['EPSG:3794'] = {extent: [32395.09,373217.65,194645.86,622710.74], units: 'm', yx: true};
-CustomProj['EPSG:3912'] = {extent: [-475760.46,363774.72,224384.21,1113039.95], units: 'm', yx: true};
+CustomProj['EPSG:2180'] = {extent: [116649.52,118322.29,806302.07,845151.24], units: 'm', yx: true};    //Poland
+CustomProj['EPSG:3006'] = {extent: [-1200000,4305696,2994304,8500000], units: 'm', yx: true};           //Sweden
+CustomProj['EPSG:3794'] = {extent: [32395.09,373217.65,194645.86,622710.74], units: 'm', yx: true};     //Slovenia new
+CustomProj['EPSG:3912'] = {extent: [-475760.46,363774.72,224384.21,1113039.95], units: 'm', yx: true};  //Slovenia old
+CustomProj['EPSG:32635'] = {extent: [-760000, 6580000, 657000, 8990000], units: 'm', yx: false};         //Norway
