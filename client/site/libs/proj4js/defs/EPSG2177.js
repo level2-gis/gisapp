1aa7e6fbef77745940b34e25b9154d94e492f0c2
uprel <uros00@gmail.com>
2017-10-21 09:41:53 +0200

    EPSG 2177 added

---
+++ client/site/libs/proj4js/defs/EPSG2177.js
@@ -0,0 +1,3 @@
+Proj4js.defs["EPSG:2177"]= "+title=Poland CS2000 zone 6 +proj=tmerc +lat_0=0 +lon_0=18 +k=0.999923 +x_0=6500000 +y_0=0 +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs";
+
+
--- client_common/customProjections.js
+++ client_common/customProjections.js
@@ -13,6 +13,7 @@ var CustomProj = [];
 
 CustomProj['EPSG:3857'] = {extent: [-20037508.34,20037508.34,20037508.34,20037508.34], units: 'm', yx: false};
 CustomProj['EPSG:2180'] = {extent: [116649.52,118322.29,806302.07,845151.24], units: 'm', yx: true};    //Poland
+CustomProj['EPSG:2177'] = {extent: [], units: 'm', yx: true};
 CustomProj['EPSG:3006'] = {extent: [-1200000,4305696,2994304,8500000], units: 'm', yx: true};           //Sweden
 CustomProj['EPSG:3794'] = {extent: [32395.09,373217.65,194645.86,622710.74], units: 'm', yx: true};     //Slovenia new
 CustomProj['EPSG:3912'] = {extent: [-475760.46,363774.72,224384.21,1113039.95], units: 'm', yx: true};  //Slovenia old
