d80a3059aeab0f42f98f5a49da20b757b494c5c5
uprel <uros00@gmail.com>
2018-01-30 08:54:01 +0100

    OSM layer 20 zoom levels by default

--- _scripts/_setup.sql
+++ _scripts/_setup.sql
@@ -436,7 +436,7 @@ SELECT pg_catalog.setval('clients_id_seq', 1, false);
 
 INSERT INTO layers VALUES (1, 'google_map', 'Google Streets', 'Google', '{type: google.maps.MapTypeId.MAP, numZoomLevels: 20, isBaseLayer: true, useTiltImages: false}');
 INSERT INTO layers VALUES (2, 'google_sat', 'Google Satellite', 'Google', '{type: google.maps.MapTypeId.SATELLITE, numZoomLevels: 20, isBaseLayer: true}');
-INSERT INTO layers VALUES (4, 'osm_mapnik', 'OpenStreetMap', 'OSM', '');
+INSERT INTO layers VALUES (4, 'osm_mapnik', 'OpenStreetMap', 'OSM', '{"numZoomLevels": 20, "serverResolutions": [156543.03390625, 78271.516953125, 39135.7584765625, 19567.87923828125, 9783.939619140625, 4891.9698095703125, 2445.9849047851562, 1222.9924523925781, 611.4962261962891, 305.74811309814453, 152.87405654907226, 76.43702827453613, 38.218514137268066, 19.109257068634033, 9.554628534317017, 4.777314267158508, 2.388657133579254, 1.194328566789627, 0.5971642833948135, 0.29858214169740677]}');
 
 
 --
