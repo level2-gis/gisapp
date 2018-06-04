e19122d601807bad5492167b746a68ce7b15bee0
uprel <uros00@gmail.com>
2018-06-04 10:04:40 +0200

    Updated Slovenian translation CSS Reference updated for Editor plugin

--- index.php
+++ index.php
@@ -231,7 +231,7 @@ if (Helpers::isValidUserProj(Helpers::getMapFromUrl())) {
                 <link rel="stylesheet" type="text/css" href="client/site/css/contextMenu.css?v=20180320"/>
 
                 <?php if ($edit) {
-                    echo '<link rel="stylesheet" type="text/css" href="plugins/editing/theme/geosilk/geosilk.css?v=20171209"/>';
+                    echo '<link rel="stylesheet" type="text/css" href="plugins/editing/theme/geosilk/geosilk.css?v=20180603"/>';
                 }?>
 
                 <script type="text/javascript" src="client_common/load.php"></script>
