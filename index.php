4ca2502740b2750d0944aae0249468398f4a7f7c
uprel <uros00@gmail.com>
2018-10-08 22:19:19 +0200

    OpenLayers2 add WFS 1.1.0 format to build (needed for QGIS Server 3 )

--- index.php
+++ index.php
@@ -257,7 +257,7 @@ if ($login_check->isValidUserProj($helpers->getMapFromUrl())) {
                 <script type="text/javascript" src="client/site/libs/ext/ux/ux-all.js?v=20180215"></script>
 
                 <script type="text/javascript" src="client/site/libs/proj4js/proj4js-compressed.js"></script>
-                <script type="text/javascript" src="client/site/libs/openlayers/OpenLayers.js?v=20171213"></script>
+                <script type="text/javascript" src="client/site/libs/openlayers/OpenLayers.js?v=20181008"></script>
 
 <!--                FOR DEBUGGING-->
 <!--                <script type="text/javascript" src="client/site/libs/openlayers/OpenLayers_debug.js"></script>-->
