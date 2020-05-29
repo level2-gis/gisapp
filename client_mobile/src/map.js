743663ccc53d62790deb27bbe5e01ea2582f67b9
uros <uros00@gmail.com>
2020-05-29 20:12:21 +0200

    Mobile client: commenting pinchzoom setting (reverting to previous state)

--- client_mobile/src/map.js
+++ client_mobile/src/map.js
@@ -148,11 +148,11 @@ Map.createMap = function() {
     layers: [],
     target: 'map',
     view: new ol.View(Config.map.viewOptions),
-    interactions: ol.interaction.defaults({pinchZoom: false}).extend([
-          new ol.interaction.PinchZoom({
-              constrainResolution: true // force zooming to a integer zoom
-          })
-    ]),
+    // interactions: ol.interaction.defaults({pinchZoom: false}).extend([
+    //       new ol.interaction.PinchZoom({
+    //           constrainResolution: true // force zooming to a integer zoom
+    //       })
+    // ]),
     controls:[]
   });
 
