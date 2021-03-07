0711d86dfb4cb05c1f169416e0fdcd49f8aa610a
uros <uros00@gmail.com>
2021-03-07 20:57:30 +0100

    Fix openlayers2 issue Unable to preventDefault inside passive...

--- client/site/js/QGISExtensions.js
+++ client/site/js/QGISExtensions.js
@@ -1875,8 +1875,7 @@ Ext.override(Ext.dd.DragTracker, {
         if(!this.active){
             if(Math.abs(s[0]-xy[0]) > this.tolerance || Math.abs(s[1]-xy[1]) > this.tolerance){
                 this.triggerStart(e);
-            }
-            else{
+            } else {
                 return;
             }
         }
@@ -1886,3 +1885,67 @@ Ext.override(Ext.dd.DragTracker, {
     }
 });
 
+//fix for Openlayers2 preventdefault chrome issue
+//https://stackoverflow.com/questions/55955171/preventing-unable-to-preventdefault-inside-passive-event-listener-error-within
+const eventListenerOptionsSupported = () => {
+    let supported = false;
+
+    try {
+        const opts = Object.defineProperty({}, 'passive', {
+            get() {
+                supported = true;
+            }
+        });
+
+        window.addEventListener('test', null, opts);
+        window.removeEventListener('test', null, opts);
+    } catch (e) {
+    }
+
+    return supported;
+}
+
+const defaultOptions = {
+    passive: false,
+    capture: false
+};
+const supportedPassiveTypes = [
+    'scroll', 'wheel',
+    'touchstart', 'touchmove', 'touchenter', 'touchend', 'touchleave',
+    'mouseout', 'mouseleave', 'mouseup', 'mousedown', 'mousemove', 'mouseenter', 'mousewheel', 'mouseover'
+];
+const getDefaultPassiveOption = (passive, eventName) => {
+    if (passive !== undefined) return passive;
+
+    return supportedPassiveTypes.indexOf(eventName) === -1 ? false : defaultOptions.passive;
+};
+
+const getWritableOptions = (options) => {
+    const passiveDescriptor = Object.getOwnPropertyDescriptor(options, 'passive');
+
+    return passiveDescriptor && passiveDescriptor.writable !== true && passiveDescriptor.set === undefined
+        ? Object.assign({}, options)
+        : options;
+};
+
+const overwriteAddEvent = (superMethod) => {
+    EventTarget.prototype.addEventListener = function (type, listener, options) {
+        const usesListenerOptions = typeof options === 'object' && options !== null;
+        const useCapture = usesListenerOptions ? options.capture : options;
+
+        options = usesListenerOptions ? getWritableOptions(options) : {};
+        options.passive = getDefaultPassiveOption(options.passive, type);
+        options.capture = useCapture === undefined ? defaultOptions.capture : useCapture;
+
+        superMethod.call(this, type, listener, options);
+    };
+
+    EventTarget.prototype.addEventListener._original = superMethod;
+};
+
+const supportsPassive = eventListenerOptionsSupported();
+
+if (supportsPassive) {
+    const addEvent = EventTarget.prototype.addEventListener;
+    overwriteAddEvent(addEvent);
+}
