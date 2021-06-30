05090923b94415f30419ce3c97c72a4269536d6f
Uroš <uros00@gmail.com>
2021-06-30 19:58:01 +0200

    fix some UTM projections

--- client/site/libs/proj4js/defs/EPSG32632.js
+++ client/site/libs/proj4js/defs/EPSG32632.js
@@ -1,1 +1,1 @@
-Proj4js.defs["EPSG:32632"]= "+proj=utm +zone=32 +ellps=WGS84 +datum=WGS84 +units=m +no_defs";
+Proj4js.defs["EPSG:32632"]= "+title=UTM32N +proj=utm +zone=32 +ellps=WGS84 +datum=WGS84 +units=m +no_defs";
--- client/site/libs/proj4js/defs/EPSG32635.js
+++ client/site/libs/proj4js/defs/EPSG32635.js
@@ -1,2 +1,2 @@
-Proj4js.defs["EPSG:32635"]= "+title-UTM35N +proj=utm +zone=35 +ellps=WGS84 +datum=WGS84 +units=m +no_defs";
+Proj4js.defs["EPSG:32635"]= "+title=UTM35N +proj=utm +zone=35 +ellps=WGS84 +datum=WGS84 +units=m +no_defs";
 
