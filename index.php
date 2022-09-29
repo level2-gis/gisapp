174a7b0818e0c83219c7a72bb48766c19b599f0c
Uroš Preložnik <uros00@gmail.com>
2022-09-29 09:03:10 +0200

    sodo logotipi plugin

--- index.php
+++ index.php
@@ -278,7 +278,7 @@ if ($login_check->setUserProj($helpers->getMapFromUrl())) {
     <link rel="stylesheet" type="text/css" href="client/site/css/popup.css?v=20200228"/>
     <link rel="stylesheet" type="text/css" href="client/site/css/layerOrderTab.css?v=20200405"/>
     <link rel="stylesheet" type="text/css" href="client/site/css/contextMenu.css?v=20191222"/>
-    <link rel="stylesheet" type="text/css" href="client/site/css/style-gray.css?v=20220927"/>
+    <link rel="stylesheet" type="text/css" href="client/site/css/style-gray.css?v=20220929"/>
 
     <?php if ($edit) {
                     echo '<link rel="stylesheet" type="text/css" href="plugins/editing/theme/geosilk/geosilk.css?v='.$editVer.'"/>';
---
+++ plugins/sodo_logotipi/js/1_gui.js
@@ -0,0 +1,40 @@
+/**
+ *
+ * Configuration of simple EQWC plugin which consists of new button in top menu with own icon, tooltip and action.
+ * It is independend from other buttons.
+ *
+ * To use this plugin:
+ * 1. Copy /plugins/_template directory to /plugins/simpleaction and make following changes in simpleaction/js/1_gui.js
+ * 2. Make sure you are running latest database version (18 or higher)
+ * 3. Adjust icon in /img/icon.png
+ * 4. Adjust tooltip below
+ * 5. Adjust url in newWindow function below
+ *
+ * You can have more such plugins, each with own icon and action
+ * To create new plugin from this template
+ * 1. Copy /plugins/_template directory to /plugins/yourpluginname and make following changes in yourpluginname/js/1_gui.js
+ * 2. Insert plugin to database with: INSERT INTO plugins(name) VALUES ('yourpluginname');
+ * 3. Change Eqwc.plugins["simpleaction"] lines to Eqwc.plugins["yourpluginname"]
+ * 4. Change plugin id from id: 'simpleActionBtn' to something unique and reference it in customToolbarLoad
+ * 5. Also look at steps 2-5 above
+ *
+ */
+
+Eqwc.plugins["sodo_logotipi"] = {};
+Eqwc.plugins["sodo_logotipi"].customToolbarLoad = function() {
+
+    var id = 'sodo_logotipi';
+    var header = Ext.getCmp('GisBrowserPanel');
+
+    header.addTool({
+        id: id,
+        //qtip: 'Write tooltip',
+        //handler: function (event, toolEl, panel) {
+        //    var url = "http://...";
+        //    window.open(url);
+        //}
+    });
+
+    var cnt = Object.keys(header.tools).length-1;
+    header.tools[id].setRight(20+(32*cnt));
+};
